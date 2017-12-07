c4ff6bd8427ca9d3f27e9a90f05c835ab3c31865
Michael Albert <albertm@students.svsd410.org>
2017-12-07 15:02:45 -0800

    name fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CurrentDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CurrentDrive.java
@@ -3,19 +3,15 @@ package org.firstinspires.ftc.teamcode;
 /**
  * Created by michaelalbert on 10/15/17.
  */
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorController;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.Range;
 
 /**
  * Created by michaelalbert on 10/14/17.
  */
-@TeleOp(name = "Michael - USE THIS GUYS ", group = "TeleOp")
+@TeleOp(name = "Michael - USE THIS GUYS SINGLE", group = "TeleOp")
 //@Disabled
 public class CurrentDrive extends LinearOpMode {
     private DcMotor leftmotor;
@@ -46,18 +42,18 @@ public class CurrentDrive extends LinearOpMode {
             //servo left max= 0; close = 140
             //servo right max= 145; close = 0
 
-            armpowerup= gamepad2.right_trigger ;
+            armpowerup= gamepad1.right_trigger ;
             armmotor.setPower(armpowerup);
-            armpowerdown= -gamepad2.left_trigger/2;
+            armpowerdown= -gamepad1.left_trigger/2;
             armmotor.setPower(armpowerdown);
 
 
-            if(gamepad2.a){
+            if(gamepad1.a){
 //            open
                 armclaw.setPosition(0.00);
             }else {
 //            close
-                if (gamepad2.b) {
+                if (gamepad1.b) {
                     armclaw.setPosition(0.28);
                 }
             }
@@ -65,8 +61,8 @@ public class CurrentDrive extends LinearOpMode {
 
 
 
-            rightmotor.setPower(gamepad1.right_stick_y/1.1);
-            leftmotor.setPower(gamepad1.left_stick_y/1.1);
+            rightmotor.setPower(gamepad1.right_stick_y/1.5);
+            leftmotor.setPower(gamepad1.left_stick_y/1.5);
             telemetry.update();
             idle();
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NOTCURRENT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NOTCURRENT.java
@@ -3,26 +3,33 @@ package org.firstinspires.ftc.teamcode;
 /**
  * Created by michaelalbert on 10/15/17.
  */
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
 /**
  * Created by michaelalbert on 10/14/17.
  */
-@TeleOp(name = "Michael - USE THIS GUYS SINGLE", group = "TeleOp")
+@TeleOp(name = "Michael - USE THIS GUYS ", group = "TeleOp")
 //@Disabled
-public class TankDriveWithEncodersSingle extends LinearOpMode {
+public class NOTCURRENT extends LinearOpMode {
     private DcMotor leftmotor;
     private DcMotor rightmotor;
     private DcMotor armmotor;
     private Servo armclaw;
 
+
     @Override
     public void runOpMode() throws InterruptedException {
         double armpowerdown;
         double armpowerup;
+        boolean fast;
+
         leftmotor = hardwareMap.dcMotor.get("leftmotor");
         rightmotor = hardwareMap.dcMotor.get("rightmotor");
 
@@ -33,7 +40,7 @@ public class TankDriveWithEncodersSingle extends LinearOpMode {
 
 
         waitForStart();
-
+        fast = false;
         armclaw.setPosition(0.00);
 
         while ((opModeIsActive())) {
@@ -42,18 +49,18 @@ public class TankDriveWithEncodersSingle extends LinearOpMode {
             //servo left max= 0; close = 140
             //servo right max= 145; close = 0
 
-            armpowerup= gamepad1.right_trigger ;
+            armpowerup= gamepad2.right_trigger ;
             armmotor.setPower(armpowerup);
-            armpowerdown= -gamepad1.left_trigger/2;
+            armpowerdown= -gamepad2.left_trigger/2;
             armmotor.setPower(armpowerdown);
 
 
-            if(gamepad1.a){
+            if(gamepad2.a){
 //            open
                 armclaw.setPosition(0.00);
             }else {
 //            close
-                if (gamepad1.b) {
+                if (gamepad2.b) {
                     armclaw.setPosition(0.28);
                 }
             }
@@ -61,8 +68,8 @@ public class TankDriveWithEncodersSingle extends LinearOpMode {
 
 
 
-            rightmotor.setPower(gamepad1.right_stick_y/1.5);
-            leftmotor.setPower(gamepad1.left_stick_y/1.5);
+            rightmotor.setPower(gamepad1.right_stick_y/1.1);
+            leftmotor.setPower(gamepad1.left_stick_y/1.1);
             telemetry.update();
             idle();
         }
